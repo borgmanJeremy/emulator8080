b3df3f508e01d93d78f9aec5083cb9439b0babb1
Jeremy Borgman <borgman.jeremy@pm.me>
2019-12-10 16:17:23 -0600

    pushing tests

--- emulator/test/test_8080_add.cpp
+++ emulator/test/test_8080_add.cpp
@@ -19,8 +19,9 @@ TEST_CASE("Simple Adds")
   test_scenario.addInstruction(0x85);
   test_scenario.addInstruction(0x86);
   test_scenario.addInstruction(0x87);
+  test_scenario.addInstruction(0xC6);
 
-  SECTION("Result is zero, z=1 s=0 p=1 ac=0 cy=0")
+  SECTION("Zero / Parity Flag Test")
   {
     test_scenario.modifyRegisterAllInstructions(REG_NAME::PC, 1,
                                                 MOD_SCOPE::SOLUTION);
@@ -28,6 +29,10 @@ TEST_CASE("Simple Adds")
                                             MOD_SCOPE::SOLUTION);
     test_scenario.modifyFlagAllInstructions(FLAG_NAME::P, 1,
                                             MOD_SCOPE::SOLUTION);
+
+    cpu.memory_[0x01] = 0x00;
+    test_scenario.modifyRegisterByInstruction(REG_NAME::PC, 0x02,
+                                              MOD_SCOPE::SOLUTION, 0xC6);
 
     for (auto const &scenario : test_scenario.test_instructions_)
     {
@@ -45,7 +50,7 @@ TEST_CASE("Simple Adds")
     }
   }
 
-  SECTION("Result is zero, z=1 s=? p=? ac=1 cy=1")
+  SECTION("Carry and Auxillary Carry Flag Test")
   {
     // Configure similar setup
     for (auto &scenario : test_scenario.test_instructions_)
@@ -58,6 +63,7 @@ TEST_CASE("Simple Adds")
                                                 MOD_SCOPE::SETUP);
 
     cpu.memory_[0x00] = 0x01;
+    cpu.memory_[0x01] = 0x01;
     test_scenario.modifyRegisterByInstruction(REG_NAME::H, 0,
                                               MOD_SCOPE::SETUP_SOLUTION, 0x86);
     test_scenario.modifyRegisterByInstruction(REG_NAME::L, 0,
@@ -76,6 +82,19 @@ TEST_CASE("Simple Adds")
                                             MOD_SCOPE::SOLUTION);
 
     // Configure overrides
+    // Need to override adding A to A, cant hit same flags on this as others
+    test_scenario.modifyFlagByInstruction(FLAG_NAME::Z, 0, MOD_SCOPE::SOLUTION,
+                                          0x87);
+    test_scenario.modifyFlagByInstruction(FLAG_NAME::P, 0, MOD_SCOPE::SOLUTION,
+                                          0x87);
+    test_scenario.modifyFlagByInstruction(FLAG_NAME::S, 1, MOD_SCOPE::SOLUTION,
+                                          0x87);
+    test_scenario.modifyRegisterByInstruction(REG_NAME::A, 0xFE,
+                                              MOD_SCOPE::SOLUTION, 0x87);
+
+    test_scenario.modifyRegisterByInstruction(REG_NAME::PC, 0x02,
+                                              MOD_SCOPE::SOLUTION, 0xC6);
+
     for (auto const &scenario : test_scenario.test_instructions_)
     {
       SECTION(std::to_string(scenario.instruction_))
@@ -92,14 +111,13 @@ TEST_CASE("Simple Adds")
     }
   }
 
-  // This set setup also checks that each addition has no side channel effects
+  // This test setup also checks that each addition has no side channel effects
   // on other registers
-  SECTION("Result is 11 (except a), z=0 s=0 p=0 ac=0 cy=0")
+  SECTION("Test that adding from reg x does not affect reg y")
   {
     // Configure similar setup
     test_scenario.modifyRegisterAllInstructions(REG_NAME::A, 1,
                                                 MOD_SCOPE::SETUP);
-
     test_scenario.modifyRegisterAllInstructions(REG_NAME::PC, 1,
                                                 MOD_SCOPE::SOLUTION);
     test_scenario.modifyRegisterAllInstructions(REG_NAME::A, 11,
